3edb7d3ac9bc55919a85af0b3762f7417b767ca6
Brian McGue <mcgue.brian@gmail.com>
2014-01-04 12:08:43 -0800

    added pausing ability. changed functionality, looks cleaner

--- snake/snake_ui.js
+++ snake/snake_ui.js
@@ -4,6 +4,7 @@
 	var UI = SnakeGame.UI = function (el) {
 		this.$el = $(el);
 		this.keyMapping = {
+			32: 'pause',
 			37: 'W',
 			38: 'N',
 			39: 'E',
@@ -11,30 +12,48 @@
 		};
 	};
 
+	UI.prototype.beginInterval = function () {
+		this.interval = setInterval(this.step.bind(this), 100);
+	}
+
 	UI.prototype.draw = function () {
 		this.$el.html($('<pre>').html(this.board.render()));
 	};
 
+	UI.prototype.handleKey = function() {
+		var dir = this.keyMapping[event.which];
+		if (dir === 'pause') {
+			this.handlePause();
+		} else if (this.interval && dir !== undefined) {
+			this.board.snake.turn(dir);
+		}
+	};
+
+	UI.prototype.handlePause = function () {
+		if (this.interval) {
+			this.pause();
+		} else {
+			this.beginInterval();
+		}
+	};
+
 	UI.prototype.installKeyHandlers = function () {
-		var ui = this;
-		$(window).on('keydown', function(event) {
-			var dir = ui.keyMapping[event.which];
-			if (dir !== undefined) {
-				ui.board.snake.turn(dir);
-			}
-		});
+		$(window).on('keydown', this.handleKey.bind(this));
+	};
+
+	UI.prototype.pause = function () {
+		clearInterval(this.interval);
+		delete this.interval;
 	};
 
 	UI.prototype.start = function () {
 		this.board = new SnakeGame.Board();
 		this.installKeyHandlers();
-		setInterval(this.step.bind(this), 200);
+		this.beginInterval();
 	};
 
 	UI.prototype.step = function () {
-		console.log("stepping");
 		this.board.snake.move();
 		this.draw();
 	};
-
 })(this);
